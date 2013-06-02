bd55df43a30f77f90b88180f89f94da83dfdb047
stormslowly@gmail.com <stormslowly@gmail.com>
2013-06-02 21:48:58 +0800

    create books migrate

---
+++ db/migrate/20130602133614_create_books.rb
@@ -0,0 +1,17 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+
+      t.string :title,:limit=>255,:null=>false
+      t.integer :publisher_id ,:null=>false
+      t.datetime :published_at
+      t.string :isbn, :limit=>13,:unique=>true
+      t.text :blurb
+      t.integer :page_counter
+      t.float :price
+
+      t.timestamps
+
+    end
+  end
+end
