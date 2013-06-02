f01a76d920756dd47ad18de53ef55700ca6fa225
stormslowly@gmail.com <stormslowly@gmail.com>
2013-06-02 19:57:07 +0800

    and unqiue publish validate

--- test/controllers/admin/publishers_controller_test.rb
+++ test/controllers/admin/publishers_controller_test.rb
@@ -18,7 +18,7 @@ class Admin::PublishersControllerTest < ActionController::TestCase
 
   test "should create admin_publisher" do
     assert_difference('Admin::Publisher.count') do
-      post :create, admin_publisher: { name: "#{@admin_publisher.name}_other" }
+      post :create, admin_publisher: { name: "just a new  press" }
     end
 
     assert_redirected_to admin_publisher_path(assigns(:admin_publisher))
