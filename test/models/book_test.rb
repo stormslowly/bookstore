d7b4ba62af24ad04bfef818d059de5ee409f9034
stormslowly@gmail.com <stormslowly@gmail.com>
2013-06-03 00:51:35 +0800

    add asserts about book new errors

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -7,8 +7,16 @@ class BookTest < ActiveSupport::TestCase
 
   test "should failed when new without parameters" do
     book = Book.new
+
     assert_equal false, book.save
-    assert_equal 7,book.errors.size
+    assert_equal 7 ,book.errors.size
+    #book.errors.o( :title)
+    assert  book.errors[:title]
+    assert  book.errors[:admin_publisher]
+    assert  book.errors[:admin_authors]
+    assert  book.errors[:page_counter]
+    assert  book.errors[:price]
+    assert  book.errors[:isbn]
   end
 
 end
