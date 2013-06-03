106afe0ce5a0694602f2a56385594f9ca4c74ad8
stormslowly@gmail.com <stormslowly@gmail.com>
2013-06-03 20:38:05 +0800

    some clean

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -10,7 +10,6 @@ class BookTest < ActiveSupport::TestCase
 
     assert_equal false, book.save
     assert_equal 7 ,book.errors.size
-    #book.errors.o( :title)
     assert  book.errors[:title]
     assert  book.errors[:admin_publisher]
     assert  book.errors[:admin_authors]
@@ -19,12 +18,12 @@ class BookTest < ActiveSupport::TestCase
     assert  book.errors[:isbn]
   end
   test "should success when new with proper parameters" do
-    book = Book.new(:title=>"great ruy",
+    book = Book.new(:title=> 'great ruy',
                     :admin_publisher => Admin::Publisher.find(1),
                     :admin_authors => Admin::Author.all,
                     :published_at => Time.now ,
                     :isbn => 123-456-999,
-                    :blurb => "good book for rubist",
+                    :blurb => 'good book for rubist',
                     :page_counter =>55,
                     :price =>5.5)
 
